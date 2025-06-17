143c7bfa2fd4636106d42c7b3a963dfdc4ed4f0b
fschildt <florian.schildt@protonmail.com>
2025-06-17 13:48:46 +0200

    export cmake compile commands in CMakeLists.txt

--- compile.sh
+++ compile.sh
@@ -7,10 +7,10 @@ cd build
 
 if [[ "$0" == "Release" ]]; then
     echo "Release"
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../cmake
+    cmake -DCMAKE_BUILD_TYPE=Release ../cmake
 else
     echo "Debug"
-    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../cmake
+    cmake -DCMAKE_BUILD_TYPE=Debug ../cmake
 fi
 
 make
